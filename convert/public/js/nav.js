80fea5b4bf2e28f961479c498bd07b5a122913ed
NelsenJ <nelsen.johnathan@gmail.com>
2024-10-23 21:03:09 +0700

    js complete

--- convert/public/js/nav.js
+++ convert/public/js/nav.js
@@ -10,24 +10,32 @@ window.onload = function () {
     menu_btn.classList.toggle("is-active");
     mobile_menu.classList.toggle("is-active");
   });
+  let darkmode = localStorage.getItem("darkmodeNebula");
+  const themeSwitch = document.getElementById("theme-switch");
+
+  const enableDarkmode = () => {
+    document.body.classList.add("darkmode");
+    localStorage.setItem("darkmodeNebula", "active");
+  };
+
+  const disableDarkmode = () => {
+    document.body.classList.remove("darkmode");
+    localStorage.setItem("darkmodeNebula", null);
+  };
+
+  if (darkmode === "active") enableDarkmode();
+
+  themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmodeNebula')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+  });
 };
 
-let darkmode = localStorage.getItem("darkmodeNebula");
-const themeSwitch = document.getElementById("theme-switch");
-
-const enableDarkmode = () => {
-  document.body.classList.add("darkmode");
-  localStorage.setItem("darkmodeNebula", "active");
-};
-
-const disableDarkmode = () => {
-  document.body.classList.remove("darkmode");
-  localStorage.setItem("darkmodeNebula", null);
-};
-
-if (darkmode === "active") enableDarkmode();
-
-themeSwitch.addEventListener("click", () => {
-  darkmode = localStorage.getItem('darkmodeNebula')
-  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+window.addEventListener('DOMContentLoaded', () => {
+  const storedName = localStorage.getItem('name');
+  if (storedName) document.getElementById('name').innerHTML = storedName;
 });
+
+
+
+
